1ae89906018b368666303eb8d5ac13d11cfda2c6
Francisco Paulino de Queiroz <franciscopaulino.q@gmail.com>
2026-06-15 23:21:42 -0300

    Initial commit

---
+++ src/test/java/io/github/franciscopaulinoq/zelaseveriano/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.franciscopaulinoq.zelaseveriano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
