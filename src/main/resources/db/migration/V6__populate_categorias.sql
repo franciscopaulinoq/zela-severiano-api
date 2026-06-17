b37ee07574b5893b68b7dfbbe499ceb9cdf6b374
Francisco Paulino de Queiroz <franciscopaulino.q@gmail.com>
2026-06-16 21:59:37 -0300

    chore: create migration to populate categorias table with initial data

---
+++ src/main/resources/db/migration/V6__populate_categorias.sql
@@ -0,0 +1,6 @@
+INSERT INTO "categorias" ("nome", "icone_nome") VALUES
+                                                    ('Iluminação', 'lightbulb'),
+                                                    ('Água/Esgoto', 'faucet-drip'),
+                                                    ('Limpeza', 'trash'),
+                                                    ('Asfalto/Buraco', 'road'),
+                                                    ('Outro problema', 'circle-exclamation');
